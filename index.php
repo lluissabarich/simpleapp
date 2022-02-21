bf86e03fd4917283838ad00577308f2a624e7f02
lluissabarich <sabarich@gmail.com>
2022-02-21 18:01:09 +0100

    Update index.php
    - 2 change

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body> 
 	<h1>PHP Test</h1>
-		<p><b>An Example of PHP in Action for this demo</b></p>
+		<p><b>An Example of PHP in Action for this demo - 2</b></p>
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information</h2> 
